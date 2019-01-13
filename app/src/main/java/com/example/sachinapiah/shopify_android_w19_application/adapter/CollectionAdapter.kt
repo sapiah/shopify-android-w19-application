75257e4e750d6133cc0202b17831b6c1753e2314
Sachin Apiah <sachinappiah20@gmail.com>
2019-01-12 20:23:14 -0500

    Create recyclerView for collections page

---
+++ app/src/main/java/com/example/sachinapiah/shopify_android_w19_application/adapter/CollectionAdapter.kt
@@ -0,0 +1,43 @@
+package com.example.sachinapiah.shopify_android_w19_application.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.sachinapiah.shopify_android_w19_application.R
+import kotlinx.android.synthetic.main.collection_list_recycler_view_item.view.*
+import java.util.*
+
+
+class CollectionAdapter(private val items: ArrayList<String>, context: Context) :
+    RecyclerView.Adapter<CollectionViewHolder>() {
+
+    private val inflater = LayoutInflater.from(context)
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    override fun onBindViewHolder(holder: CollectionViewHolder, position: Int) {
+        holder.onBind(items[position])
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CollectionViewHolder {
+        val view = inflater.inflate(R.layout.collection_list_recycler_view_item, parent, false)
+        return CollectionViewHolderValid(view)
+    }
+}
+
+abstract class CollectionViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    abstract fun onBind(item: String)
+}
+
+class CollectionViewHolderValid(view: View) : CollectionViewHolder(view) {
+    private val tvCollectionTitle: TextView = view.tv_collection_title
+
+    override fun onBind(item: String) {
+        tvCollectionTitle.text = item
+    }
+}
